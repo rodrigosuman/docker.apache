c1eb719ce6581de6b2f5fe6e19c944b154c4cbeb
Rodrigo Suman <rodrigosuman18@gmail.com>
2020-06-03 16:28:20 -0300

    inital commit

---
+++ public_html/casthood/precache-manifest.0a55c27f2f3f6a18e5a51d9a149236b0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d41e56f586308660c213e5a8f67513",
+    "url": "/casthood/index.html"
+  },
+  {
+    "revision": "a0249f25708e51e96aff",
+    "url": "/casthood/static/css/main.acb41cb2.chunk.css"
+  },
+  {
+    "revision": "045d23cc8c772b222c7c",
+    "url": "/casthood/static/js/2.fe6636c2.chunk.js"
+  },
+  {
+    "revision": "a0249f25708e51e96aff",
+    "url": "/casthood/static/js/main.d906a3c0.chunk.js"
+  },
+  {
+    "revision": "2adfbd53b28751b405a9",
+    "url": "/casthood/static/js/runtime-main.85e5fe23.js"
+  },
+  {
+    "revision": "54135489ecfe443c89dea791b850f616",
+    "url": "/casthood/static/media/Android_icon_CH.54135489.png"
+  },
+  {
+    "revision": "af5825ef84fe785de9d5b837685c0230",
+    "url": "/casthood/static/media/Grupo 2277.af5825ef.png"
+  },
+  {
+    "revision": "bce29050269e3a49912cec365a3e094f",
+    "url": "/casthood/static/media/Grupo 2278.bce29050.png"
+  },
+  {
+    "revision": "455476de0c4d49cd13e659d3f44a84db",
+    "url": "/casthood/static/media/il_Anuncio.455476de.png"
+  },
+  {
+    "revision": "1fa789088491d573d6d0f91489ba2cd3",
+    "url": "/casthood/static/media/il_Bem-vindo.1fa78908.png"
+  },
+  {
+    "revision": "4296068f5742d0b5ee65dbf0e82599b7",
+    "url": "/casthood/static/media/il_Chame_equipe.4296068f.png"
+  },
+  {
+    "revision": "a90f7c446642be09a770a6fded7a5a2b",
+    "url": "/casthood/static/media/il_canalreivindicado.a90f7c44.png"
+  },
+  {
+    "revision": "d23cfd76cc553c830076eb96f36fe51a",
+    "url": "/casthood/static/media/il_caster-1.d23cfd76.png"
+  },
+  {
+    "revision": "dee49c84f171503f1144e1b733994c1f",
+    "url": "/casthood/static/media/il_caster.dee49c84.png"
+  },
+  {
+    "revision": "e2b9674ed35e2af45619976620054636",
+    "url": "/casthood/static/media/il_checklist.e2b9674e.png"
+  },
+  {
+    "revision": "6bb5a4dfabfa9906f14fe776f29c66bd",
+    "url": "/casthood/static/media/il_personalizar.6bb5a4df.png"
+  },
+  {
+    "revision": "35af589d0520db936e9986a2ea39395d",
+    "url": "/casthood/static/media/il_problemas.35af589d.png"
+  },
+  {
+    "revision": "5ae6085524a77c739848de2b6bdcca75",
+    "url": "/casthood/static/media/il_senha.5ae60855.png"
+  }
+]);
